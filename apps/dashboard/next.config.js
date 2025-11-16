b1e7b9152e9ecb5eb56b4f1132696b57a5d96c08
YannickT1Pro <zannellay@gmail.com>
2025-11-16 15:25:17 +0100

    w

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['@kodari/database', '@kodari/shared'],
-  // experimental.serverActions: true, <- entfernt, Next.js 14+ aktiviert automatisch
+  // experimental.serverActions: true, <- entfernen
 };
-  
